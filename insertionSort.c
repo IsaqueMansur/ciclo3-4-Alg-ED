ad21574b6cff6ea4b27973ae26491dfb5642e452
Isaque Mansur de Oliveira <isaque.oliveira@artvac.local>
2022-11-29 16:29:51 -0300

    create option for generate randoms numbers sorted by crescent module

--- insertionSort.c
+++ insertionSort.c
@@ -2,7 +2,7 @@
 //bibliotecas
 #include <math.h>
 #include <stdio.h>
-
+#include <stdlib.h>
 void insertionSort(int arr[], int size){ //funcao sequenciadora
     int i, j, key; //contadores
     for (i = 1; i < size; i++) { //laço de repetição de acordo com o tamanho do array
@@ -19,12 +19,22 @@ void insertionSort(int arr[], int size){ //funcao sequenciadora
 void printArray(int arr[], int size){ //funcao que imprime o resultado
     int i; //contador
     for (i = 0; i < size; i++) // laço de repetição para impressão dos valores
-        printf("%d ", arr[i]); //impressão dos valores pós sequenciamento
+        printf("%d | ", arr[i]); //impressão dos valores pós sequenciamento
     printf("\n"); //impressão de espaço pós renderização
 }
 
 void main(){
-    int arr[] = { 12, 11, 13, 5, 6 }; //array a ser sequanciado
+    printf("Sequenciador de números aleatórios \n");
+    printf("Digite a quantidade de numeros alatórios que deseja gerar e trazer sequenciados (ordem crescente) \n"); //Solicita a quantidade que deseja gerar;
+    int qtd; //identificador da quantidade
+    scanf("%d", &qtd); //função para digitar quantidade
+    int arr[qtd]; //array a ser sequanciado
+    int i = 0;
+    while (qtd > 0 && i < qtd) { //criar um aleatório enquanto o indice for menor que a quantidade solicitada
+        int randomNumber = (random() / 100000); //gerando valor aleatório
+        arr[i] = randomNumber; //inserindo valor gerado ao array
+        i = i + 1; //contador++
+    }
     int size = sizeof(arr) / sizeof(arr[0]); //tamanho do array
     insertionSort(arr, size); //chamando a funcao sequenciadora
     printArray(arr, size);//chamando a funcao que imprime o resultado
